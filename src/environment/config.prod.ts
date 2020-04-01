c8980d8089692d6bcf85a09a467f3d70efa5d227
Roger Alvarez <ralvarez@brainshark.com>
2020-04-01 07:20:29 -0500

    Api Url changed

--- src/environment/config.prod.ts
+++ src/environment/config.prod.ts
@@ -1,7 +1,7 @@
 import { AppConfig } from './config';
 
 export const config: AppConfig = {
-  apiBaseUrl: 'https://healthapi.azurewebsites.net/api/',
+  apiBaseUrl: 'https://healthapp-backend.azurewebsites.net/api/',
 };
 
 export default config;
